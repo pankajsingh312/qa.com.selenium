db58d44232da37995f21c12c455ed5f8fe9887ca
Pankaj Singh <pankaj.singh5@globallogic.com>
2021-06-01 21:24:41 +0530

    Initial commit

---
+++ src/main/java/com/qa/automation/LaunchBrowser.java
@@ -0,0 +1,23 @@
+package com.qa.automation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args)
+    {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.get("https://www.google.com/");
+        driver.quit();
+
+
+    }
+}
